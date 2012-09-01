25b7367ac5f16ccf47c02d90980b9a1dbd85ceff
Amman Vedi <amman.vedi@gmail.com>
2012-09-02 01:10:57 +0200

    Annotated code (on a plane) and added image files to repo

--- Drippp_v2/DRPTableViewController.m
+++ Drippp_v2/DRPTableViewController.m
@@ -5,6 +5,8 @@
 //  Created by Amman on 20/08/2012.
 //  Copyright (c) 2012 Amman. All rights reserved.
 //
+//Provided via gitbub AS-IS open source
+//No warantee 
 
 #import "DRPTableViewController.h"
 #import "DRPCell_main.h"
@@ -44,22 +46,31 @@ int lower = 30;
 {
     [super viewDidLoad];
     
-    
-
+    //the variable "page" will increment each time a new page of the api data is loaded
+    //the value is used to pass the "page" parameter in the HTTP url request
+    //incremented after page data has been gathered so its initial value must be 2
     self.page = 2;
     
+    
+    //init arrays
+    //images: holds the images from Teaser_Url from api Json Data
+    //Urlsarray: holds all the obects that are parsed from the JSON api response
     images = [[NSMutableArray alloc] init];
     urlsarray = [[NSMutableArray alloc] init];
     
+    
+    //TO-DO thread this process, so it doesnt hang the UI
+    
     NSError *jsonerror;
-     NSData *jsondataset = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://api.dribbble.com/shots/popular?per_page=30"]];
-    
-    //NSData *jsondataset = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"dribble_response" ofType:@"json"]];
-    
+    //get data for first page
+    NSData *jsondataset = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://api.dribbble.com/shots/popular?per_page=30"]];
+    //load a dictionary with response data
     NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsondataset options:kNilOptions error:&jsonerror];
-    
+    //the actual objects containing shot data and player data are stored in an array within
+    //the "shots" JSON bject here i am putting this data in an array
     NSArray *shotsarray = [json objectForKey:@"shots"];
     
+    //for Debug
     for (int n = 1; n <= 29; n = n + 1) {
         NSLog(@"%@", [[[shotsarray objectAtIndex: n] objectForKey:@"player"] objectForKey:@"twitter_screen_name"]);
         NSLog(@"%@", [[shotsarray objectAtIndex: n] objectForKey:@"image_url"] );
@@ -68,12 +79,21 @@ int lower = 30;
        
 
     }
-
-    self.urlsarray = [[NSMutableArray alloc]
-                      initWithArray:shotsarray];
     
+    //pass the array shotsarry to the global property urlsarray
+    self.urlsarray = [[NSMutableArray alloc] initWithArray:shotsarray];
+    //create a thread and download the images
+    //asyncloadimages is the method that is called to get the initial page/JSON data set
     [NSThread detachNewThreadSelector:@selector(asyncloadimages) toTarget:self withObject:nil];
 
+    
+    //this hangs so there are no errors
+    //basically waits for the array to be loaded
+    
+    //TO-DO remove this
+    //see what errors pop up
+    //handle errors
+    //call a reload of the tableview
     while (threaddone ==false) {
         
     }
@@ -83,6 +103,7 @@ int lower = 30;
 
 - (void)viewDidUnload
 {
+    //TO-DO determine when called setnil accordingly
     
     [self setRefresh_Button:nil];
     [self setNav_item:nil];
@@ -92,6 +113,8 @@ int lower = 30;
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
+    //keep the interface portrait
+    //plans to handle any landscape orientation will come in later versions
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
@@ -105,16 +128,17 @@ int lower = 30;
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-
-    // Return the number of rows in the section.    
+    // Return the number of rows in the section.
+    //when further pages are fetched the array will become larger (mutable)
+    //so the number of rows needs to be returned asa new (larger) value
+    //this method is invoked on the call of [self.tableview reload]
     return [self.urlsarray count];
 
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-
-    
+    //set up the dynamic tableview
     
     static NSString *CellIdentifier = @"drippp_cell_main";
     DRPCell_main *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
@@ -124,12 +148,16 @@ int lower = 30;
                 initWithStyle:UITableViewCellStyleDefault
                 reuseIdentifier:CellIdentifier];
     }
+    
+    //care must be taken with what is placed in this method because
+    //1: this is on the main thread any heavy input will prevent the ui updating leading to unresponsive UI
+    //2. this method is invoked ALOT, almost everytime a cell is drawn
 
-    
+    //set the label to the name of the artist / player / creator
     cell.makeLabel.text =  [[[self.urlsarray objectAtIndex: [indexPath row]] objectForKey:@"player"] objectForKey:@"name"];
-    
-   // [cell.image_holder_main setImageWithURL:[NSURL URLWithString:[[self.urlsarray objectAtIndex:[indexPath row]] objectForKey:@"image_teaser_url"]] placeholderImage:[UIImage imageNamed:@"ph.png"]];
-    
+    //set image of the cell from the array.
+    //the array essentially acts as a cache,
+    //with the advantage that it doesnt get purged when the app enters background ect.
     [cell.image_holder_main setImage:[images objectAtIndex:[indexPath row]]];
 
     
@@ -145,40 +173,36 @@ int lower = 30;
 
 
 -(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    //all cells need to be 240px high so return 240px to be sure of this
+    //could just set in code but this allows me to be sure of the value being set 100%
     return 240.0f;
 }
 
 -(void) get_next_page{
     
 
-
+    //for debug
     NSLog(@"%@", @"Getting next page");
-    
-    
     NSError *jsongetnextpageerror;
-    
-   
-    
     NSString *urlstring;
     
+    //url for json request with html param "page" from the variable "page"
     urlstring = [NSString stringWithFormat:@"http://api.dribbble.com/shots/popular?per_page=30&page=%d", page];
-    
-    
-    
+    //data object for json data
     NSData *jsondatasetfornextpage = [NSData dataWithContentsOfURL:[NSURL URLWithString:urlstring]];
-    
-    
+    //dictionary for data
     NSDictionary *jsonfromnextpage = [NSJSONSerialization JSONObjectWithData:jsondatasetfornextpage options:kNilOptions error:&jsongetnextpageerror];
-    
+    //array for new data from page
     NSArray *nextpagearray = [jsonfromnextpage objectForKey:@"shots"];
-
+    //add the data from the new page to the end of the main daat holding array "urlsarray"
     [self.urlsarray addObjectsFromArray: nextpagearray];
-
-    
-
+    //For Debug
     NSLog(@"%@", [NSString stringWithFormat:@"%d", [self.urlsarray count]]);
     
-    
+    //this loop will download the image data for the next page of images
+    //add it to the end of the image array
+    //call an update to the progressview
     for (int x=lower; x<=lower+29; x++) {
         
         
@@ -187,43 +211,49 @@ int lower = 30;
         UIImage *imagefromasync = [UIImage imageWithData:imagedata];
         
         [images insertObject:imagefromasync atIndex:x];
-        
+        //For Debug
         NSLog(@"%u", [images count]);
-                
+        //this next line calls the methid to update the progrssview, however due to the fact that
+        //currently it is called from a "parralel" thread, so this will call it in the main thread
+        //solving the problem of UI updates not being allwed to happen anywhere except in the main thread 
         [self performSelectorOnMainThread:@selector(updateprgview) withObject:nil waitUntilDone:NO];
-        
-        //[self updateprgview];
-        
-         NSLog(@"%f", (PRGView.progress +0.03));
+        //For Debug
+        NSLog(@"%f", (PRGView.progress +0.03));
         
     }
     
-    
+    //setnil and reassign the title when the progressview has finished
+    //which by this point it will have as the loop has ended
     nav_item.titleView = nil;
     nav_item.title =@"drippp";
     
+    //re enable the button
     [Refresh_Button setEnabled:true];
     
+    //incerement the lower value
     lower += 30;
     
+    //call a reload of the tableview 
     [self.tableView reloadData];
     
+    //increment the value of "page"
     page++;
+    //For Debug
     NSLog(@"%d", page);
     NSLog(@"%@", urlstring);
 }
 
 - (IBAction)Clicked_More:(id)sender {
     
+    //ibaction methid called on the button click of the refresh button
     [Refresh_Button setEnabled:false];
-    
+    //create a progressview for display in the navigationbar
     PRGView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleBar];
-    
+    //initialize the progressview to 0 progress
     [PRGView setProgress:0.0f];
-    
+    //set the progressview to display
     nav_item.titleView = PRGView;
-    
-    
+    //create and begin a thread to get the next page 
     [NSThread detachNewThreadSelector:@selector(get_next_page) toTarget:self withObject:nil];
     
 
@@ -231,40 +261,48 @@ int lower = 30;
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     
+    //create a obect to enable referral to the destinationviewcontroller (DVC)
     DRPShotViewController *DRPSVC = [segue destinationViewController];
-
-    
+    //determine which segue has been called
     if ([segue.identifier isEqualToString: @"MainTableSegue"]) {
+        //assign property of DVC to nsdata object
+        //allows all data about a shot to be passed in one package at one time
         DRPSVC.passedData = passData;
         NSLog(@"prep segue");
     }
 }
 
 - (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    //when a user clicks a cell the object of data that corresponds to that cell is passed
     passData = [urlsarray objectAtIndex:[indexPath row]];
+    //For Debug
     NSLog(@"select row");
     return indexPath;
 }
 
 -(void) asyncloadimages{
-
+    //method that is called to download the initial data
     
     for (int x=0; x<=29; x++) {
-        
+        //create data object
         NSData *imagedata = [NSData dataWithContentsOfURL: [NSURL URLWithString:[[self.urlsarray objectAtIndex:x] objectForKey:@"image_teaser_url"]]];
-        
+        //create image
         UIImage *imagefromasync = [UIImage imageWithData:imagedata];
-        
+        //add image to image array
         [images insertObject:imagefromasync atIndex:x];
-        
+        //For Debug
         NSLog(@"%u", [images count]);
     }
     
+    //indicate that the thread has terminated/finished
     threaddone = true;
     
 }
 
 -(void)updateprgview{
+    //Method to update the ProgressView
+    //set it to increase by 1/30
+    //i.e. one thirt-eth increase for each of 30 images to download
     [PRGView setProgress: (PRGView.progress + 0.03f) animated:YES];
 }
 
